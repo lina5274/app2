e2036519b4c008112fa543ccd94ea90f21c50114
lina5274 <167440004+lina5274@users.noreply.github.com>
2024-08-06 14:34:21 +0300

    Update Seaborn.py

--- diplom/Seaborn.py
+++ diplom/Seaborn.py
@@ -7,35 +7,30 @@ import pandas as pd
 data_path = r'C:\Users\79156\Desktop\demography_paneldata_csv_19042021\data.csv'
 data = pd.read_csv(data_path, dtype=str, low_memory=False)
 
-# filtered_df = data[data['bride_age'].notna()]
-#
-# sns.histplot(filtered_df['bride_age'], bins=20, kde=True)
-# plt.title('Распределение возраста невест')
-# plt.xlabel('Возраст невесты')
-# plt.ylabel('Частота')
-# plt.show()
+# Создание фигуры с заданным размером, чтобы вместить все метки оси X
+plt.figure(figsize=(12, 10))
+filtered_df = data[data['bride_age'].notna()]
 
-# # Фильтрация данных для удаления пропущенных значений
-# filtered_df = data[data[['bride_age', 'groom_age']].notna().all(axis=1)]
-#
-# sns.scatterplot(data=filtered_df, x='bride_age', y='groom_age')
-# plt.title('Возраст невесты vs Возраст жениха')
-# plt.xlabel('Возраст невесты')
-# plt.ylabel('Возраст жениха')
-# plt.show()
 
-# Примерный датафрейм для демонстрации
-dat = {
-    'year': [2015, 2016, 2017, 2018],
-    'region': ['Region1', 'Region2', 'Region3', 'Region4'],
-    'marriages': [100, 150, 120, 130]
-}
-df_marriages = pd.DataFrame(dat)
+sns.histplot(filtered_df['bride_age'], bins=20, kde=True)
+plt.title('Распределение возраста невест')
+plt.xlabel('Возраст невесты')
+plt.ylabel('Частота')
 
-# Переформатирование датафрейма для heatmap
-df_pivot = df_marriages.pivot(index='year', columns='region', values='marriages')
+# Управление метками оси x
+plt.xticks(rotation=45)  # Вращение меток на 45 градусов для лучшей читаемости
 
-# Создание heatmap с аннотациями чисел с плавающей точкой
-sns.heatmap(df_pivot, annot=True, fmt=".1f")  # Использование ".1f" для аннотаций
-plt.title('Heatmap с аннотациями чисел с плавающей точкой')
+plt.show()
+
+# Создание фигуры с заданным размером, чтобы вместить все метки оси X
+plt.figure(figsize=(10, 9))
+# Фильтрация данных для удаления пропущенных значений
+filtered_d = data[data[['bride_age', 'groom_age']].notna().all(axis=1)]
+
+sns.scatterplot(data=filtered_d, x='bride_age', y='groom_age')
+plt.title('Возраст невесты vs Возраст жениха')
+plt.xlabel('Возраст невесты')
+plt.ylabel('Возраст жениха')
+# Управление метками оси x
+plt.xticks(rotation=45)  # Вращение меток на 45 градусов для лучшей читаемости
 plt.show()
